c634b3f1ac97163c2761d53111fa41da5826bf56
Guilherme Torres <torres.me@outlook.com>
2023-10-21 00:52:07 -0300

    feat: add initial screen

---
+++ android/app/src/main/kotlin/com/guieltorres/todo_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.guieltorres.todo_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/tasks_screen.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatelessWidget {
+  const TasksScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        backgroundColor: Colors.lightBlueAccent,
+        child: const Icon(Icons.add),
+      ),
+      backgroundColor: Colors.lightBlueAccent,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding:
+                const EdgeInsets.only(top: 60, left: 30, right: 30, bottom: 30),
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                  child: Icon(
+                    Icons.list,
+                    size: 30,
+                    color: Colors.lightBlueAccent,
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  "Todoey",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 50.0,
+                      fontWeight: FontWeight.w700),
+                ),
+                Text(
+                  "12 tasks",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+              ),
+              child: ListView(
+                padding: const EdgeInsets.only(top: 12),
+                children: [
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 12, right: 12, bottom: 4),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text("Ok"),
+                        IconButton(
+                          onPressed: () => {},
+                          icon: const Icon(Icons.check_box_outline_blank),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
